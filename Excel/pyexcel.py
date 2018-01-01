e832467e485a2c8d584c91034ad88314d6464f4f
xuetu <wangtjun68@yahoo.com>
2018-01-01 18:28:03 +0800

    python for excel

--- Excel/pyexcel.py
+++ Excel/pyexcel.py
@@ -10,18 +10,27 @@ class Excel_handle(object):
 	"""docstring for ClassName"""
 	nrows = 0
 	def __init__(self):      #can use global var
-		self.fileform = '.xlsx'
-		self.slist= []
+		self.fileform = '.xls'
+		self.Slist= []
 		self.bsdict = {}
-		self.collist = []    # need to set
+		self.collist = [u'L',u'Part-no.',u'S',u'unit',u'Unit weight   (kg)',u'Rmarks']    # need to set
 
 	def bk_sheet_list(self,filepath):
 
+		slist = []
+		rmlist = [u'Summary sheet',u'Sprache',u'UnVisible',u'Shapes',u'add',u'标准预制件索引']
 		for filename in glob.glob(filepath+'*'+self.fileform):
 			bk = xlrd.open_workbook(filename)
-			self.slist = self.slist + bk.sheet_names() 
+			slist = slist + bk.sheet_names() 
 			self.bsdict[filename] = bk.sheet_names()
-		return self.slist
+		for value in slist:
+			if rmlist[-1] in value:
+				continue
+			elif value in rmlist:
+				continue
+			else:
+				self.Slist.append(value)
+		return self.Slist
 
 	def return_bkname(self,sheet):     
 		# return the list of bkname(path+name) those has the same sheet
@@ -29,45 +38,49 @@ class Excel_handle(object):
 		# input a sheetname 
 		# ergodic the list, if have the same value ,return the path of book
 		bklst = []
-		if self.slist.count(sheet) > 1 :  	
+		if self.Slist.count(sheet) > 1 :  	
 			for key in self.bsdict.keys():
 				if sheet in self.bsdict[key]:
 					bklst.append(key)   
 			return bklst
 		else:
 			for key in self.bsdict.keys():
-				if sheet in self.bsdict[sheet]:
+				if sheet in self.bsdict[key]:
 					bklst.append(key)
 					break   
 			return bklst
-	def write_excel(self,book,nrows,new_sheet):   # find there Designated col ant write to new excel
+	def write_excel(self,book,sheet,New_sheet):   # find there Designated col ant write to new excel
 		# read the firs row of the excel
 		# ergodic the fist row list
 		# if find the same value, write to new excel
 		bk = xlrd.open_workbook(book)
-		shet = bk.sheet_by_name(sheet)
-		m = 0
+		shet = bk.sheet_by_name(sheet)  ##
+		first_row = shet.row_values(5)
+		print first_row
 		row_lst = []
-		first_row = shet.row_values(0)
-		for colname in self.collist:
-			if colname in first_row:
-				n = find_index(colname, first_row)
-				col_data = shet.col_values(n)
-				row = nrows + 1
-				for i in range(1,len(col_data)):
-					New_sheet.write(row,m,col_data[i])
-					m = m+1
-					row = row + 1
+		for index,colname in enumerate(self.collist):
+			colname = filter(lambda x : x in first_row,[colname, colname.capitalize()])
+			if colname:
+				n = self.find_index(colname[0],first_row)
+				col_data = shet.col_values(n)[0:30]
+				while '' in col_data:
+					col_data.remove('')
 				row_lst.append(len(col_data))
+				row = self.nrows + 1
+				for i in range(1,len(col_data)):
+					New_sheet.write(row,index+1,col_data[i])
+					row = row + 1
 			else:
 				continue
-		nrows = nrows + max(row_lst)-1
+		self.nrows = self.nrows + max(row_lst)-1
+		for  i  in range(1,self.nrows+1):
+			New_sheet.write(i,0,sheet)   #New_sheet.write(i,0,sheet+'/'+shet.cell(5,8))
 
 		# need to judge the max len of the col_data of every sheet
 
 	def find_index(self,value,lst):
-		for i in range(len(lst)):
-			if lst[i] == value:
+		for index in range(len(lst)):
+			if lst[index] == value:
 				return index
 
 filepath = "/home/hacker/pythonts/"
@@ -77,12 +90,12 @@ slist = mk_excel.bk_sheet_list(filepath)
 new_bk = xlwt.Workbook()
 
 for sheet in set(slist):
-	New_sheet = new_bk.add_sheet(sheet)
+	New_sheet = new_bk.add_sheet(sheet,cell_overwrite_ok=True)
 	bklst = mk_excel.return_bkname(sheet)
-	biaotou = []  ##
+	biaotou = [u'名称',u'规格',u'图纸编号（物料编号）',u'材料',u'单位',u'单重',u'备注']  ##
 	for i in range(0,len(biaotou)):  # if possible,del
-		New_sheet.write(0,i,biaotou[i])
-	nrows = 0
-	for book in bklst:
-		mk_excel.write_excel(book,nrows,New_sheet)
-filename.save(filedestination+file+".xls")
+		New_sheet.write(0,i,biaotou[i])	
+	for book in bklst:                                 ## not work
+		mk_excel.write_excel(book,sheet,New_sheet)
+	mk_excel.nrows = 0
+new_bk.save(filedestination+"test.xls")
