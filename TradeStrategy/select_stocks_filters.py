b343d7ce9221467fab690f801dae21e28de6ec9f
xuetuing <32641642+xuetuing@users.noreply.github.com>
2019-10-18 00:27:43 +0800

    update

---
+++ TradeStrategy/select_stocks_filters.py
@@ -0,0 +1,12 @@
+class SelectStocksFilters(object):
+    # EMPA指标快线上穿慢线
+    def checkEMPA(self,stock):
+        pass
+    
+    # 每股价格 < 32
+    def checkPrice(self):
+        pass
+
+    # 庄家筹码 > 85%
+    def chipOfDealer(self):
+        pass
---
+++ TradeStrategy/tradeUtils.py
@@ -0,0 +1,7 @@
+class TradeUtils(object):
+    # d周期内的最低值的值，x 为最低值 
+    def llv(self, x, d):
+        pass
+
+    def hhv(self, x, d):
+        pass
